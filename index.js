781f99c48dd5da92b9d8da6fda582eac07afcf1a
AlmosYT <almosytpixelplace@gmail.com>
2023-01-04 21:15:46 +0100

    Minor changes

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 //this project is meant to track the fuckups that my friend simon has made. it should show a realtime timer since the last fuckup.
 //it should also show the total number of fuckups that have happened over all time
 
-//load fuckups from a json file and store them in an array
-//fuckups should be an array of objects with the following properties:
-//date: the date of the fuckup (UNIX timestamp)
-//description: a description of the fuckup
-
 var fuckups = [];
 var fuckupsCount = 0;
 
@@ -44,12 +39,6 @@ function refreshFuckups() {
 
 
 function displayTimerSinceLastFuckup() {
-	//get the current time
-	//get the date of the last fuckup
-	//calculate the difference between the current time and the last fuckup
-	//display the difference in a timer on the page
-	//the timer is a span with the id "timer"
-
 	var now = new Date();
 	console.log(now);
 	console.log(fuckups[fuckups.length - 1].date)
@@ -69,11 +58,6 @@ function displayTimerSinceLastFuckup() {
 }
 
 function displaylistOfFuckups() {
-	//display the fuckups into the div with the id "fuckups"
-	//use h3 elements for the date and p elements for the description
-	//use the .innerHTML property to set the html of the div
-	//use the fuckups array to get the fuckups
-
 	var fuckupsDiv = document.getElementById("fuckups");
 
 	var innerHTML = "";
